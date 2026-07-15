a1932562a1d8dea25d5943a83956c29017e1dd11
bryan viveros <bryan_led@hotmail.com>
2026-07-15 14:34:15 -0600

    Generador de pdf casi listo

---
+++ src/generarpdf.js
@@ -0,0 +1,71 @@
+import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+
+export async function generarPDF(datos){
+
+    const respuesta = await fetch("/plantilla.pdf");
+    const bytes = await respuesta.arrayBuffer();
+
+    const pdfDoc = await PDFDocument.load(bytes);
+
+    const paginas = pdfDoc.getPages();
+    const pagina = paginas[0];
+
+    const fuente = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    pagina.drawText(datos.clave, {
+        x: 275,
+        y: 445,
+        size: 10,
+        font: fuente,
+        color: rgb(0,0,0)
+    });
+
+    pagina.drawText(datos.propietario, {
+        x: 275,
+        y: 422,
+        size: 10,
+        font: fuente,
+        color: rgb(0,0,0)
+    });
+
+     pagina.drawText(datos.direccion, {
+        x: 275,
+        y: 402,
+        size: 10,
+        font: fuente,
+        color: rgb(0,0,0)
+    });
+
+     pagina.drawText(datos.telefono, {
+        x: 275,
+        y: 382,
+        size: 10,
+        font: fuente,
+        color: rgb(0,0,0)
+    });
+
+     pagina.drawText(datos.beneficiario, {
+        x: 400,
+        y: 200,
+        size: 10,
+        font: fuente,
+        color: rgb(0,0,0)
+    });
+
+    pagina.drawText(datos.fecha, {
+        x: 444,
+        y: 679,
+        size: 10,
+        font: fuente,
+        color: rgb(0,0,0)
+    });
+
+    const pdfBytes = await pdfDoc.save();
+
+    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+
+    const url = URL.createObjectURL(blob);
+
+    window.open(url);
+
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,29 @@
 import './style.css'
+import { generarPDF } from './generarpdf';
 
+const formulario = document.querySelector(".formulario");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const fecha = new Date();
+    const fechaActual = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
+    console.log(fechaActual);
+    const datos = {
+
+    fecha: fechaActual,
+    clave: document.getElementById("clave").value,
+    propietario: document.getElementById("propietario").value,
+    direccion: document.getElementById("direccion").value,
+    beneficiario: document.getElementById("beneficiario").value,
+    telefono: document.getElementById("telefono").value
+
+    }
+
+    generarPDF(datos);
+
+}
+
+
+)
 
