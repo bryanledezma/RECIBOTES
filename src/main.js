202053a7b28a1579514dde2aae6f2c3e5e378d37
bryan viveros <bryan_led@hotmail.com>
2026-08-20 15:18:35 -0600

    mejora de diseño, pequeñas validaciones

--- src/main.js
+++ src/main.js
@@ -5,46 +5,54 @@ const formulario = document.querySelector(".formulario");
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    const fecha = new Date();
-    const fechaActual = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
-
+//----------------------Recolectar datos del formulario-----------------------------------
     let cajero = document.getElementById("cajero").value;
+    const datosCajero = getCajero(cajero); //Funcion getCajero realiza la definición de los datos del cajero: nombre, puesto y coordenada x para ubicar en documento pdf
+    
+    const datos = {
+    fecha: getDate(),//funcion getDate define fecha de hoy
+    clave: document.getElementById("clave").value.trim().toUpperCase(),
+    propietario: document.getElementById("propietario").value.trim().toUpperCase(),
+    direccion: document.getElementById("direccion").value.trim().toUpperCase(),
+    beneficiario: document.getElementById("beneficiario").value.trim().toUpperCase(),
+    telefono: document.getElementById("telefono").value.trim(),
+    cajero: datosCajero.nombre,
+    puesto: datosCajero.puesto,
+    x: datosCajero.x
+    }
+    generarPDF(datos);
+})
+
+//--------FUNCIONES--------------
+function getDate(){
+     const fecha = new Date();
+    const fechaActual = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
+    return fechaActual
+}
+function getCajero(cajero){
+    console.log("cajero: ", cajero);
+    let nombre;
     let puesto;
     let x = 165; //coordenada predeterminada si el puesto es CAJERO
     switch(cajero){
-        case "4": cajero = "BLANCA IDALIA VELAZQUEZ HERNANDEZ";
+        case "4": nombre = "BLANCA IDALIA VELAZQUEZ HERNANDEZ";
                 puesto = "DIRECTORA DE CATASTRO";
                 x = 132 //Coordenada si el puesto es directora
                 break;
-        case "6": cajero = "BRYAN DANIEL LEDEZMA VIVEROS"
+        case "6": nombre = "BRYAN DANIEL LEDEZMA VIVEROS"
                 puesto = "CAJERO"
                 break;
-        case "7": cajero = "LAZARO JACOBO SEGURA MORENO"
+        case "7": nombre = "LAZARO JACOBO SEGURA MORENO"
                 puesto = "CAJERO"
                 break;
     }
-
-    const datos = {
-
-    fecha: fechaActual,
-    clave: document.getElementById("clave").value,
-    propietario: document.getElementById("propietario").value,
-    direccion: document.getElementById("direccion").value,
-    beneficiario: document.getElementById("beneficiario").value,
-    telefono: document.getElementById("telefono").value,
-    cajero: cajero,
-    puesto: puesto,
-    x:x
-    }
-
-    generarPDF(datos);
-
+    return {
+        nombre,
+        puesto,
+        x
+    };
 }
-
-
-)
-
+//-------------FUNCION PARA LIMPIAR DATOS DEL FORMULARIO---------------------
 const btnLimpiar = document.getElementById("btnLimpiar");
 btnLimpiar.addEventListener("click", limpiarFormulario);
 
